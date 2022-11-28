040e8b2976a0883fee2a98da0b02e24cb6f53417
Frans701 <franswahyu7@gmail.com>
2022-11-28 19:59:31 +0800

    add link to search page

--- src/components/SearchCard.jsx
+++ src/components/SearchCard.jsx
@@ -217,7 +217,7 @@ function SearchCard() {
                       </div>
                     </div>
                   </div>
-                  <Link to="/">
+                  <Link to="/search">
                     <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                       Search
                     </button>
@@ -400,7 +400,7 @@ function SearchCard() {
                       </div>
                     </div>
                   </div>
-                  <Link to="/">
+                  <Link to="/search">
                     <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                       Search
                     </button>
