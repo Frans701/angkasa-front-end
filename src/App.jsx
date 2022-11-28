67072b9213cd3ac7ddc67b5838e495db51f9b827
Frans701 <franswahyu7@gmail.com>
2022-11-28 16:47:11 +0800

    add search flight page

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,17 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
+import Search from "./pages/Search";
 
 export default function App() {
   return (
     <>
       <Routes>
-        <Route path ="/" element ={<Home/>}/>
-        <Route path ="/login" element ={<Login/>}/>
-        <Route path ="/register" element ={<Register/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/search" element={<Search />} />
       </Routes>
     </>
-  )
+  );
 }
