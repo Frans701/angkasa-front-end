a7d95fa0e57c7f579edf9f8dc5ef540c2286e46b
FransVirgawan <77884241+Frans701@users.noreply.github.com>
2022-11-30 21:24:55 +0800

    Features frans (#11)
    * clean footer
    
    * tabs buttom
    
    * tabs buttom
    
    * clean code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,26 +4,17 @@ import Footer from "../components/Footer";
 import SearchCard from "../components/SearchCard";
 
 function Home() {
-  let [changeText, setChangeText] = useState(true);
-  const handleChange = (id) => {
-    return setChangeText(id);
-  };
   return (
     <>
       <Navbar />
       <section className="w-full relative">
         <img
           className="w-full object-cover h-[550px]"
-          src="https://images.unsplash.com/photo-1606768666853-403c90a981ad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"
+          src="https://images.unsplash.com/photo-1436491865332-7a61a109cc05?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1474&q=80"
           alt=""
         />
         <div className="flex justify-center">
           <SearchCard />
-          <div>
-            <button onClick={() => handleChange(true)}>click benar</button>
-            <button onClick={() => handleChange(false)}>click salah</button>
-            {changeText ? <>benar</> : <>salah</>}
-          </div>
         </div>
       </section>
       <Footer />
