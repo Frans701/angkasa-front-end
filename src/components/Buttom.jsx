ee9bb4d98ff849b68f460869aa8804649ab0e704
Frans701 <franswahyu7@gmail.com>
2022-12-04 01:03:06 +0800

    add responsive search list

--- src/components/Buttom.jsx
+++ src/components/Buttom.jsx
@@ -3,8 +3,7 @@ import React from "react";
 function Buttom({ type = "submit", color, children, onPress, width }) {
   const variant = {
     blue: `${width} text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-[24px] py-2.5 focus:outline-none focus:ring-blue-800`,
-    yellow:
-      "bg-yellow-300 hover:bg-yellow-400 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm py-2.5 focus:outline-none focus:ring-yellow-400 px-[48px] text-blue-500",
+    yellow: `${width} bg-yellow-300 hover:bg-yellow-400 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm py-2.5 focus:outline-none focus:ring-yellow-400 px-[48px] text-blue-500`,
   };
 
   const buttonColor = variant[color || "blue"];
