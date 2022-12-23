4b4530e14288599e2ded2d9437a3996d539fce8c
AdiSyahrul <adisyahrul27@gmail.com>
2022-12-24 04:45:01 +0700

    fix store js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducers from "./reducers";
+import airportReducer from "./reducers/airportReducer";
 import authReducer from "./reducers/authReducer";
+import seatClassReducer from "./reducers/seatClassReducer";
+import flightReducer from "./reducers/flightReducer";
+
 
 const store = configureStore({
   reducer: {
     rootReducers,
     auth : authReducer,
+    airport : airportReducer,
+    seatClass : seatClassReducer,
+    flight : flightReducer,
   }
   //   devTools: process.env.NODE_ENV === "development",
 });
