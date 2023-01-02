457a6f6b8d8e677e7939c50f0158b35ad66705da
FransVirgawan <77884241+Frans701@users.noreply.github.com>
2023-01-02 15:29:00 +0800

    Responsive website (#47)
    * done responsive homepage
    
    * done responsive flights menu
    
    * responsive web flights

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -35,13 +35,22 @@ const Cards = ({ fligts, loading, passenger }) => {
           return (
             <li
               key={flight.id}
-              className="mb-[16px] flex flex-col items-start gap-[16px] px-[40px] py-[24px] rounded-lg drop-shadow-lg bg-white"
+              className="mb-[16px] flex flex-col items-start gap-[16px] md:px-[40px] px-[16px] py-[24px] rounded-lg drop-shadow-lg bg-white"
             >
               <div className="flex flex-col w-full gap-[24px]">
-                <div>{flight.airplane.airline.name}</div>
-                <div className="flex xl:flex-row flex-col justify-between xl:items-center w-full items-start gap-[16px] xl:gap-0">
-                  <div className="flex flex-row gap-[16px] xl:items-center">
-                    <div className="">
+                <div className="flex flex-row items-center gap-[8px]">
+                  <div className="md:hidden">
+                    <img
+                      className="md:w-[80px] w-[64px] border p-[8px] rounded-lg"
+                      src={flight.airplane.airline.logo}
+                      alt=""
+                    />
+                  </div>
+                  {flight.airplane.airline.name}
+                </div>
+                <div className="flex md:flex-row flex-col justify-between md:items-center w-full items-start gap-[32px] md:gap-0">
+                  <div className="flex flex-row gap-[16px] md:items-center w-full">
+                    <div className="hidden md:block">
                       <img
                         className="w-[80px] border p-[8px] rounded-lg"
                         src={flight.airplane.airline.logo}
@@ -49,29 +58,39 @@ const Cards = ({ fligts, loading, passenger }) => {
                       />
                     </div>
                     <div className="flex flex-col gap-[8px] items-start">
-                      <h1 className="font-bold text-xl">{flight.std.hours}</h1>
-                      <span className="text-lg">{flight.fromAirport.iata}</span>
+                      <h1 className="font-bold md:text-xl text-base">
+                        {flight.std.hours}
+                      </h1>
+                      <span className="md:text-lg text-sm">
+                        {flight.fromAirport.iata}
+                      </span>
                     </div>
                     <div className="flex flex-row items-center w-[100px]">
                       <div className="flex flex-col items-center gap-[8px] w-full">
-                        <span className="text-sm text-gray-300">
+                        <span className="md:text-sm text-xs text-gray-300">
                           {flight.estimated}
                         </span>
                         <span className="border-b-2 w-full"></span>
-                        <span className="text-sm text-gray-300">Langsung</span>
+                        <span className="md:text-sm text-xs text-gray-300">
+                          Directly
+                        </span>
                       </div>
                       <div>
                         <PaperAirplaneIcon className="h-6 w-6 text-gray-300" />
                       </div>
                     </div>
                     <div className="flex flex-col gap-[8px] items-start">
-                      <h1 className="font-bold text-xl">{flight.sta.hours}</h1>
-                      <span className="text-lg">{flight.toAirport.iata}</span>
+                      <h1 className="font-bold md:text-xl text-base">
+                        {flight.sta.hours}
+                      </h1>
+                      <span className="md:text-lg text-sm">
+                        {flight.toAirport.iata}
+                      </span>
                     </div>
                   </div>
                   <div>
                     <div>
-                      <h1 className="text-2xl font-bold text-blue-500">
+                      <h1 className="md:text-2xl text-xl font-bold text-blue-500">
                         {flight.class.price.formatted}
                         <span className="text-base text-gray-300 font-medium">
                           /pax
@@ -80,7 +99,7 @@ const Cards = ({ fligts, loading, passenger }) => {
                     </div>
                   </div>
                 </div>
-                <div className="w-full flex justify-between xl:items-center xl:flex-row flex-col gap-[8px] xl:gap-0">
+                <div className="w-full flex justify-between md:items-center md:flex-row flex-col gap-[8px] md:gap-0">
                   <div className="flex flex-row gap-[24px] items-center text-blue-500">
                     <button
                       onClick={() => {
@@ -91,7 +110,7 @@ const Cards = ({ fligts, loading, passenger }) => {
                         }
                       }}
                       type="button"
-                      className="cursor-pointer"
+                      className="cursor-pointer md:text-base text-sm"
                       aria-controls="mobile-menu"
                       aria-expanded="false"
                     >
@@ -129,7 +148,7 @@ const Cards = ({ fligts, loading, passenger }) => {
                       <div className="flex flex-row gap-[40px]">
                         <div className="flex flex-col py-[16px]">
                           <span className="w-[80px] text-lg font-semibold">
-                            <h1 className="font-bold text-xl">
+                            <h1 className="font-bold md:text-xl text-base">
                               {flight.std.hours}
                             </h1>
                           </span>
@@ -137,12 +156,12 @@ const Cards = ({ fligts, loading, passenger }) => {
                         </div>
                         <div className="border-l-2"></div>
                         <div className="flex flex-col py-[16px]">
-                          <span className="font-semibold text-lg">
+                          <span className="font-semibold md:text-lg text-base">
                             {" "}
                             {flight.fromAirport.city} ({flight.fromAirport.iata}
                             )
                           </span>
-                          <span className="text-gray-500">
+                          <span className="text-gray-500 text-sm md:text-base">
                             {flight.fromAirport.name}
                           </span>
                         </div>
@@ -152,37 +171,37 @@ const Cards = ({ fligts, loading, passenger }) => {
                           {flight.estimated}
                         </span>
                         <div className="border-l-2"></div>
-                        <div className="xl:border rounded-md xl:py-[16px] xl:px-[24px] flex-col flex gap-[16px] xl:bg-blue-500/5 xl:w-[50%]">
+                        <div className="md:border rounded-md md:py-[16px] md:px-[24px] flex-col flex gap-[16px] md:bg-blue-500/5 md:w-[50%]">
                           {" "}
-                          <div className="flex xl:flex-row flex-col xl:items-center gap-[8px]">
+                          <div className="md:flex md:flex-row flex-col md:items-center gap-[8px] hidden">
                             <img
                               className="w-[40px] border p-[8px] rounded bg-white"
                               src={flight.airplane.airline.logo}
                               alt=""
                             />
-                            <span className="text-xs xl:text-base">
+                            <span className="text-xs md:text-base">
                               {flight.flightNumber} | {flight.class.type}
                             </span>
                           </div>
-                          {/* <div className="font-semibold xl:text-base text-xs">
+                          {/* <div className="font-semibold md:text-base text-xs">
                             Kabin: 7 kg, bagasi: 20 kg
                           </div> */}
                         </div>
                       </div>
                       <div className="flex flex-row gap-[40px]">
                         <div className="flex flex-col py-[16px]">
-                          <span className="w-[80px] text-lg font-semibold">
+                          <span className="w-[80px] md:text-lg text-base font-semibold">
                             {flight.sta.hours}
                           </span>
                           <span className="text-gray-500">{`${toDate[1]} ${toDate[0]}`}</span>
                         </div>
                         <div className="border-l-2"></div>
                         <div className="flex flex-col py-[16px]">
-                          <span className="font-semibold text-lg">
+                          <span className="font-semibold md:text-lg text-base">
                             {" "}
                             {flight.toAirport.city} ({flight.toAirport.iata})
                           </span>
-                          <span className="text-gray-500">
+                          <span className="text-gray-500 text-sm md:text-base">
                             {flight.toAirport.name}
                           </span>
                         </div>
